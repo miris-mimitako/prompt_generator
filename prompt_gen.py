c298f78c8547af291dba48d4a48ace6da2d48183
miris-mimitako <miris.mikitako@gmail.com>
2022-11-15 23:19:58 +0900

    new

---
+++ prompt_gen.py
@@ -0,0 +1,69 @@
+import glob
+import itertools
+
+class Prompt_Generator:
+  def __init__(self) -> None:
+    pass
+
+  def main(self):
+    with open("./sorce.txt","r") as f:
+      origin_text = f.readlines()
+
+    swich_loc = []
+    swich_text_all = []
+    for text in origin_text:
+      swich_text = []
+      if "|" in text: 
+        swich_loc.append(1)
+        swich_text = text.split("|")
+        swich_text_all.append(swich_text)
+      else:
+        swich_loc.append(0)
+
+    if not swich_text_all==[]: comb_list = self.create_list(swich_text_all)
+
+    result_text_list = []
+    for c_list in comb_list:
+      counter = 0
+      gen_text = ""
+      for text,loc_ in zip(origin_text, swich_loc):
+        if loc_: 
+          text=c_list[counter]
+          counter +=1
+        if "\n" in text: text=text[:-1] 
+        if "*" in text: text = self.strong_prompt(text=text)
+        if "/" in text: text = self.weak_prompt(text=text)
+        gen_text += text + ", "
+      else:
+        gen_text = gen_text[:-2]
+        result_text_list.append(gen_text)
+    
+    with open("./prompt.txt","w") as wf:
+      for index, writer in enumerate(result_text_list): 
+        wf.write(writer)
+        if index != len(result_text_list)-1: wf.write("\n")
+
+  def create_list(self,l_text):
+    result_list = list(itertools.product(*l_text))
+    print(result_list)
+    return result_list
+
+
+  def strong_prompt(self,text):
+    count_ = text.count("*")
+    text = text.replace("*","")
+    new_text = "("*count_ + text + ")"*count_
+    return new_text
+
+  def weak_prompt(self,text):
+    count_ = text.count("/")
+    text = text.replace("/","")
+    new_text = "["*count_ + text + "]"*count_
+    return new_text
+
+  def __del__(self):
+    pass
+
+if __name__=="__main__":
+  PG = Prompt_Generator()
+  PG.main()
